4cdb807776cc7a657acca4fb4b188804a464731c
Jonas Nord Jacobsen <jona07d9@tietgen.dk>
2021-08-30 12:44:51 +0200

    Search subfolders in DocumentsFolderSearch

--- SuperSearcher/SearchEngines/DocumentsFolderSearch/DocumentsFolderSearch.cs
+++ SuperSearcher/SearchEngines/DocumentsFolderSearch/DocumentsFolderSearch.cs
@@ -15,21 +15,24 @@ namespace SuperSearcher.SearchEngines.DocumentsFolderSearch
         /// <summary>
         /// The path of the documents folder.
         /// </summary>
-        private readonly string DocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private readonly string DocumentsPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         public string SearchLocationName => "Documents Folder";
 
         /// <summary>
-        /// Searches for files and folders that are directly in the documents folder.
+        /// Searches for files and folders that are in the documents folder or any of it's subfolders.
         /// </summary>
         /// <param name="searchText">The text to search for.</param>
         /// <param name="maxResults">The maximum number of results to return.</param>
         /// <returns>A SearchEngineResults object containing the search results.</returns>
         public Task<SearchEngineResults> Search(string searchText, int maxResults)
         {
-            SearchEngineResults searchEngineResults = new() { SearchLocationName = SearchLocationName };
+            SearchEngineResults searchEngineResults = 
+                new() { SearchLocationName = SearchLocationName };
 
-            foreach (string entry in Directory.EnumerateFileSystemEntries(DocumentsPath, $"*{searchText}*"))
+            foreach (string entry in Directory.EnumerateFileSystemEntries(
+                DocumentsPath, $"*{searchText}*", SearchOption.AllDirectories))
             {
                 if (searchEngineResults.SearchResults.Count >= maxResults)
                 {
@@ -38,7 +41,8 @@ namespace SuperSearcher.SearchEngines.DocumentsFolderSearch
 
                 string name = entry[(DocumentsPath.Length + 1)..];
                 string path = Path.Combine(DocumentsPath, name);
-                searchEngineResults.SearchResults.Add(new DocumentsFolderSearchResult() { Name = name, Path = path});
+                searchEngineResults.SearchResults.Add(
+                    new DocumentsFolderSearchResult() { Name = name, Path = path});
             }
 
             return Task.FromResult(searchEngineResults);
