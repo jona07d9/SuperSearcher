0e03fdca72070fb72af951d3a7dd167d7bed6c6a
Jonas Nord Jacobsen <jona07d9@tietgen.dk>
2021-08-31 13:30:05 +0200

    Confirm resetting statistics in SuperSearcherWPF

---
+++ SuperSearcherWPF/ConfirmedButton.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SuperSearcherWPF
+{
+    /// <summary>
+    /// Interaction logic for ConfirmedButton.xaml
+    /// </summary>
+    public partial class ConfirmedButton : Button
+    {
+        /// <summary>
+        /// The message in the confirmation MessageBox.
+        /// </summary>
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(ConfirmedButton), new PropertyMetadata(""));
+        /// <summary>
+        /// The caption in the confirmation MessageBox.
+        /// </summary>
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register("Caption", typeof(string), typeof(ConfirmedButton), new PropertyMetadata(""));
+
+        /// <summary>
+        /// The message in the confirmation MessageBox.
+        /// </summary>
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        /// <summary>
+        /// The caption in the confirmation MessageBox.
+        /// </summary>
+        public string Caption
+        {
+            get => (string)GetValue(CaptionProperty);
+            set => SetValue(CaptionProperty, value);
+        }
+
+        /// <summary>
+        /// Initializes the component.
+        /// </summary>
+        public ConfirmedButton()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Shows a MessageBox asking to confirm the click.
+        /// </summary>
+        protected override void OnClick()
+        {
+            MessageBoxResult result = MessageBox.Show(Message, Caption,
+                MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                base.OnClick();
+            }
+        }
+    }
+}
